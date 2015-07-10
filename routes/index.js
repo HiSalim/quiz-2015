37af9b0db40fd8c920e4cc04e12bb33031a5965f
HiSalim <hisalim@users.noreply.github.com>
2015-07-10 17:30:15 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login',  sessionController.new); 		// formulario login
@@ -27,10 +28,13 @@ router.post('/quizes/create', 				sessionController.loginRequired, quizControlle
 router.get('/quizes/:quizId(\\d+)/edit', 	sessionController.loginRequired, quizController.edit);
 router.put('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',		sessionController.loginRequired, quizController.destroy);
+
 // Definición de Creditos
 router.get('/author', 						quizController.author);
 
+// Definición de rutas de comentari
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
